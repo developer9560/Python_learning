aa943007a750a5849fd84a28e757562a1975a53f
developer9560 <developer95608@gmail.com>
2025-04-17 23:24:07 +0530

    using and learing about built-in module

---
+++ basics/pro3_use_of_builtIn_method.py
@@ -0,0 +1,17 @@
+# built in program that make suse of built -in mathmatical functions
+
+
+print(abs(-25)) # 25 it returns the absolute value of a number
+print(pow(2,4)) # 16 it returns the value of x raised to the power y (x^y)
+print(round(3.14)) # 3 it rounds a floating point number to the nearest integer
+print(round(3.14,1)) # 3.1 it rounds a floating point number to the specified number of decimal places
+
+print(min(10,20,30,40,50)) # 10 it returns the smallest of the input values
+print(max(10,20,30,40,50)) # 50 it returns the largest of the input values
+print(sum([1,2,3,4,5])) # 15 it returns the sum of all the input values
+print(sum([1,2,3,4,5],10)) # 25 it returns the sum of all the input values plus an optional starting value
+print(len("hello")) # 5 it returns the length of a string or a list
+
+print(divmod(17,3)) # (5,2) it returns a tuple containing the quotient and remainder of dividing two numbers
+print(bin(64)) # 0b1000000 it returns the binary representation of a number
+print(hex(64)) # 0x40 it returns the hexadecimal representation of a number
